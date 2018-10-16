7ff2940d0e208394baff7d3d3246a67b687b50c2
Rubens Martins <rubensszm@gmail.com>
2018-10-16 06:04:42 -0300

    Projeto de testes adicionado com dependencias Nuget e primeiro teste passando

---
+++ Test/ControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using S10E2.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test 
+{
+    public class ControllerTests 
+    {
+        [Fact]
+        public void VerifyIndexType() 
+        {
+            var controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
